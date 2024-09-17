91ccc1927ea40bcae00e202b63c5960779ecd077
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-09-17 20:26:03 -0300

    Teste gitignore

--- app_installation/local_define.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('GLPI_VAR_DIR', '/mnt/efs_glpi/files');
-define('GLPI_LOG_DIR', '/mnt/efs_glpi/logs');
--- scripts/start-service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Comando para iniciar o serviço ECS ou qualquer outra ação de ativação
-echo "Iniciando o serviço ECS..."
-
-# Exemplo de comando para rodar uma nova task
-aws ecs update-service --cluster "cluster-${SHORTNAME}" --service "service-${SHORTNAME}" --desired-count 1
-
-echo "Serviço iniciado com sucesso."
